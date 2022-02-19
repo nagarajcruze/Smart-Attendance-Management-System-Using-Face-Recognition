ae46298535c77185b22d3045e1251ee306c4a88f
Nagaraj Cruze <rajnagaraj386@gmail.com>
2022-02-20 00:49:44 +0530

    Minor Changes

--- Capture_Images.py
+++ Capture_Images.py
@@ -1,15 +1,23 @@
 import cv2
+import os
+import time
 
 cap = cv2.VideoCapture(0)
 count = 1
 
+current_working_dir = os.getcwd()
+pictures_dir = current_working_dir+'\\pics'
+if not os.path.exists(pictures_dir):
+    os.mkdir(pictures_dir)
+
 face_id = input("enter your Register Number: ")
 
-for _ in range(20):
+for _ in range(5):
     ret ,image = cap.read()
-    cv2.imshow('frame', image)
+    cv2.imshow('Please Look into this FRAME', image)
     cv2.imwrite("pics/"+face_id+".jpg", image)
     print("%d --> successfull "%count)
+    time.sleep(1)
     count +=1
     if cv2.waitKey(5) & 0xFF == ord('q'):
         break
